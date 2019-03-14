eb0781ac983d4ae3f2544bf246c56e0b53ef7725
Noe <VaescoNoe@gmail.com>
2019-03-13 23:53:45 -0600

    Se crea POJO de entidad persona

---
+++ src/main/java/beans/dominio/Persona.java
@@ -0,0 +1,86 @@
+package beans.dominio;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Persona {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_persona")
+	private int idPersona;
+
+	@Column(nullable = false, length = 50)
+	private String nombre;
+
+	@Column(name = "apellido_paterno", nullable = false, length = 50)
+	private String apePaterno;
+
+	@Column(name = "apellido_materno", length = 50)
+	private String apeMaterno;
+
+	@Column(nullable = false, length = 50)
+	private String email;
+
+	@Column(length = 50)
+	private String telefono;
+
+	public int getIdPersona() {
+		return idPersona;
+	}
+
+	public void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApePaterno() {
+		return apePaterno;
+	}
+
+	public void setApePaterno(String apePaterno) {
+		this.apePaterno = apePaterno;
+	}
+
+	public String getApeMaterno() {
+		return apeMaterno;
+	}
+
+	public void setApeMaterno(String apeMaterno) {
+		this.apeMaterno = apeMaterno;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [idPersona=" + idPersona + ", nombre=" + nombre + ", apePaterno=" + apePaterno + ", apeMaterno="
+				+ apeMaterno + ", email=" + email + ", telefono=" + telefono + "]";
+	}
+
+}
